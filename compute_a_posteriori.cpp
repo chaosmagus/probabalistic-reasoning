39d3b71cd19b4680e86ac9cc63d38679add273d1
Toby Crocker <tcrocker@mymail.mines.edu>
2019-04-23 23:58:42 -0600

    get command line arg, init h prior values

--- compute_a_posteriori.cpp
+++ compute_a_posteriori.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+    //check number of arguments
     if(argc != 2){
          cout << "Incorrect number of arguments\n"; 
          exit(1);
     }
-    string observations(argv[1]); 
+    
+    //get observations
+    string observations(argv[1]);
+    double totalObs = observations.size();
+    
+    //set hypothesis prior probablilities
+    double h1Prior(0.1), h2Prior(0.2), h3Prior(0.4), h4Prior(0.2), h5Prior(0.1);
+    
+
     return 0;
 }
 
