9b525b43b03e442456dcc7729bd66fd8178672d5
Toby Crocker <tcrocker@mymail.mines.edu>
2019-04-27 20:58:13 -0600

    Not the most elegant, but it works.  Good practice with fprintf as well.

--- compute_a_posteriori.cpp
+++ compute_a_posteriori.cpp
@@ -1,67 +1,43 @@
 #include "compute_a_posteriori.h"
 
 using namespace std;
-void pHypothesis(int h_number, char obs_flavor, int obsCount, vector<double> &h_prob, vector<double> &pcherry);
-double pFlavor(int h_number, char flavor, int obsCount, vector<double> &h_prob, vector<double> &pcherry);
-
-//probability of next flavor P(F(j+1) = f(j+1) | F(j) = f(j))
-double pFlavor(int h_number, char flavor, int obsCount, vector<double> &h_prob, vector<double> &pcherry){
-    double p_flavor;
-    //check terminal condition
-    if(obsCount == 0) return 0.5;
-    
-    if(toupper(flavor) == 'C'){ 
-        for(int i = 0; i < pcherry.size(); i++) p_flavor += (pcherry[i] * h_prob[i]);
-    }
-    if(toupper(flavor) == 'L') {
-        for(int i = 0; i < pcherry.size(); i++) p_flavor += ((1 - pcherry[i]) * h_prob[i]);
-    }
-    
-    return p_flavor;    
-} 
-
-//probability of hypothesis: P(h | FLavor = flavor)
-void pHypothesis(int h_number, char obs_flavor, int obsCount, vector<double> &h_prob, vector<double> &pcherry){
-    double p_hypothesis;
-    int h(h_number);
-    //check terminal condition
-   // if(obsCount == 0) {
-        if(toupper(obs_flavor) == 'C'){ 
-           p_hypothesis = (h_prob[h] * pcherry[h]/pFlavor(h_number, obs_flavor, obsCount, h_prob, pcherry));
-            h_prob[h] = p_hypothesis;
-        }
-        if(toupper(obs_flavor) == 'L') {
-            p_hypothesis = (h_prob[h] * (1 - pcherry[h])/pFlavor(h_number, obs_flavor, obsCount, h_prob, pcherry));
-            h_prob[h] = p_hypothesis;
-        }
-    //} 
-    
-}
+void update_h_prior(vector<double> &h_prob, vector<double> &pcherry, double &p_cherry, char obs);
 
 int main(int argc, char* argv[]){
     //check number of arguments
-    if(argc != 2){
+    if(argc > 2){
          cout << "Incorrect number of arguments\n"; 
          exit(1);
     }
-    
+    string observations;
     //get observations
-    string observations(argv[1]);
-    double totalObs = observations.size();
+    if(argc == 2) observations.assign(argv[1], strlen(argv[1]));
+    else observations = "";
+    int totalObs = observations.size();
     
     //store prior probablilities
     vector<double> h_prior{0.1, 0.2, 0.4, 0.2, 0.1};
-    vector<double> p_cherry{1, 0.75, 0.5, 0.25, 0};
-   int j(0);
-    for(auto c : observations){ 
-        for(int i = 0; i < 5; i++){
-            pHypothesis(i, c, j, h_prior, p_cherry); 
-        }
-        cout << "For observation " << j << " " << c << endl;     
-        for(auto d : h_prior) cout << "d: "  << d << endl;
-        j++;
+    vector<double> p_cherry_given_h{1, 0.75, 0.5, 0.25, 0};
+    double p_cherry(0.5);
+    for(auto c : observations){
+        update_h_prior(h_prior, p_cherry_given_h, p_cherry, c);
     }
+    //print to file
+    FILE *fw;
+    fw = fopen("results.txt", "w");
+    fprintf(fw, "Observation sequence Q: %s \nLength of Q: %d\n\n", observations.c_str(), totalObs);
+    for(int ix = 0; ix < h_prior.size(); ix++) fprintf(fw,"P(h%d|Q) = %f\n",(ix + 1), h_prior[ix]);
+    fprintf(fw,"Probability that the next candy will be C, given Q: %f\n", p_cherry); 
+    fprintf(fw,"Probability that the next candy will be L, given Q: %f\n", (1 - p_cherry)); 
     
     return 0;
+}
+
+void update_h_prior(vector<double> &h_prob, vector<double> &pcherry, double &p_cherry, char obs){
+    int ix(0);
+    if(toupper(obs) == 'C') for(ix = 0; ix < h_prob.size(); ix++) h_prob[ix] *= (pcherry[ix]/p_cherry);
+    else for(ix = 0; ix < h_prob.size(); ix++) h_prob[ix] *= ((1 - pcherry[ix])/(1 - p_cherry));
+    p_cherry = 0;
+    for(int ix = 0; ix < h_prob.size(); ix++) p_cherry += (h_prob[ix]*pcherry[ix]);
 }
 
